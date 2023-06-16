55a0eac905109016b106afd46e07d035e151e8ad
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-16 03:20:06 +0100

    low

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
 * main -print the string with puts function
+*
 * Descripcion : A c program that prints with put funcion
+*
 * Return : 0
 */
-int main(void){
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
