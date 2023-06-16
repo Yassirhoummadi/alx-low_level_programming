2b9b98e2a273f25e9a56f88ba1a74005ed98c8ee
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-16 03:44:36 +0100

    low

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -print the string with puts function
+*
+* Descripcion : A c program that prints with put funcion
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
