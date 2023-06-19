ad55a744d0aa6c2f11d592f9d8723f682aae5de2
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-19 23:01:26 +0100

    updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print all alpha
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+
+	return (0);
+}
