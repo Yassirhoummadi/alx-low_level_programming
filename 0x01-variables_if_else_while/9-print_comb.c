52e492f7cb934cabf06fd86398192249f79be10f
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-20 00:30:20 +0100

    updated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print 0, 1,
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int numb = 0;
+
+	while (numb <= 9)
+	{
+		putchar(numb + 48);
+		if (numb != 9)
+		{
+			putchar();
+			putchar();
+		}
+		numb++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
