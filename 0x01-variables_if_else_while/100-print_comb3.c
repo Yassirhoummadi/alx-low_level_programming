e22b7d80da927f0673f1c12083db05f9095705e6
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-20 00:48:32 +0100

    updated

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print digital
+*
+* Return: 0
+*
+**/
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit <= 9)
+	{
+		digit2 = 0;
+		while (digit <= 9)
+		{
+			if (digit != digit2 && digit1 < digit2)
+			{
+				putchar('digital1 + 48');
+				putchar('digital2 + 48');
+
+				if (digital1 + digital2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+			digital2++;
+		{
+		digital1++;
+	}
+	putchar('\n');
+
+
+}
