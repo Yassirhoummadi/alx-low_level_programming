07b01a5d991bfa662469d112d818e80fd82dbc95
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-21 02:50:44 +0100

    up

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function that add to diffrenct numbers
+ * @x: int value
+ * @y: type int
+ *
+ * Return: result
+ *
+ */
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
