46f9703d0bc222db0f764d021818d8233cd73962
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-21 02:19:30 +0100

    up

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_alphabet - function that print the alphabet in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	int i
+		;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
