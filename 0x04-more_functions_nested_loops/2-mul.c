0fab3851016e82bb739e8953dd656f55eacb1547
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-22 22:37:59 +0100

    upper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - function that prints the last digit of a number.
+ * @a: int value
+ * @b: int value
+ *
+ * Return: the value of the last digit
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
