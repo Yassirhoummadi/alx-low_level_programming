93449fc8992f83aa1b02cf28233f6e26238b5906
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-30 03:15:00 +0100

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,3 @@
-#include <stdio.h>
-#include "main.h"
-
-/**
- *_strlen - returns the length of a string
- *@str:a string of length to be returned
- *Return: returns the length of a string
- */
-int _strlen(char *str)
-{
-	int length = 0;
-
-	while (*str)
-	{
-		str++;
-		length++;
-	}
-
-	return (length);
-
-}
-
-
-/**
 #include "main.h"
 #include <stdio.h>
 
