140aba5773ac8d72c971672877d91e3fc6746053
Yassirhoummadi <yassirhoummadixd@gmail.com>
2023-06-30 03:13:43 +0100

    updated

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@str:a string of length to be returned
+ *Return: returns the length of a string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str)
+	{
+		str++;
+		length++;
+	}
+
+	return (length);
+
+}
+
+
+/**
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
